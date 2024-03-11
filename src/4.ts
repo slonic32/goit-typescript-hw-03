6633b900971eeb271fb8bd3cab5be5597ed7c43e
Artem Hordiyevskyy <artem2500@gmail.com>
2024-03-11 21:57:24 +0200

    homework 3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,47 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(k: Key) {
+    this.key = k;
+  }
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  private tenants: Person[];
+  public comeIn(p: Person): void {
+    if (this.door) {
+      this.tenants.push(p);
+    }
+  }
+  constructor(k: Key) {
+    this.door = false;
+    this.key = k;
+  }
+  public abstract openDoor(k: Key);
+}
+
+class MyHouse extends House {
+  public openDoor(k: Key) {
+    if (k === this.key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +50,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
